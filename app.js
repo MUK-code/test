9d478f542a2283d7e9ac095e057186c27d2e6d77
MUK-code <engr.usman8855@gmail.com>
2025-05-31 20:16:07 +0500

    updated UI

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from DevOps!'));
+app.get('/', (req, res) => res.send('Hello from DevOps! from Usman'));
 app.listen(3000, () => console.log('App running on port 3000'));
